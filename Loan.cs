8d939721ded813d5285b1a606b87b17348983acc
J.D.C.P. NANDASIRI <chathuni.prabodha@gmail.com>
2025-11-04 15:39:53 +0530

    Create Loan.cs

---
+++ Loan.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace LibrarySystem.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int MemberId { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
